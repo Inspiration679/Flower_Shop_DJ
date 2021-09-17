b4e504663e3df8c8c14207c9cfd75838ce154827
Inspiration679 <taraskalaida@gmail.com>
2021-09-18 02:07:02 +0300

    Сделал 2 страницу, добавил отдельную страницу для товара

--- FLowers/urls.py
+++ FLowers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import showFlowers
+from FLowers import views
 
 urlpatterns = [
-    path("", showFlowers, name="show_flowers")
+    path("", views.showFlowers, name="show_flowers"),
+    path("<int:flower_id>",views.ShowNecessaryFlower,name="show_necessary_flower")
 ]
--- FLowers/views.py
+++ FLowers/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Flowers
+
 
 def showFlowers(request):
-    return render(request,"Flowers.html",{"title":"Flowers","path":"css/MainPage/style.css"})
+    flowers = Flowers.objects
+
+    return render(request, "Flowers.html",
+                  {"title": "Shop", "path": "css/Flowers/style.css", "all_path": "css/All.css", "flowers": flowers})
+def ShowNecessaryFlower(request, flower_id):
+    page=get_object_or_404(Flowers,pk=flower_id)
+    return render(request,"Flower_Page.html",{"flower_info":page,"path": "css/Necessary_Flower/style.css", "all_path": "css/All.css",})
