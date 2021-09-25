256c0d65b3f752754f7137341e3c8c7bc1cc4071
Inspiration679 <54864677+Inspiration679@users.noreply.github.com>
2021-09-25 18:56:20 +0300

    Delete Bucket directory

--- Bucket/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import showBucket
-urlpatterns=[
-    path("",showBucket.as_view(),name="show_bucket")
-]
--- Bucket/views.py
+++
@@ -1,8 +0,0 @@
-from django.views import View
-from Home.views import MixinView
-
-
-# Create your views here.
-class showBucket(MixinView, View):
-    template = "bucket.html"
-    context = {"title": "Bucket", "path": "css/Home/style.css"}
