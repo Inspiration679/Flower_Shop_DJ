6e664b1ff5c603e5e535d47cbb93938747a2df05
Inspiration679 <taraskalaida@gmail.com>
2021-10-10 22:34:25 +0300

    Добавил фильтры товара и коментированние

--- Products/admin.py
+++ Products/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+# Регистрация модели товара
 admin.site.register(Product)
+
+# Регистрация модели категорий
 admin.site.register(ProductTag)
+
+# Регистрация модели товара
 admin.site.register(Composition)
 admin.site.register(Brand)
 admin.site.register(Size)
